09ecf528638de76b7ff6d39fc0e7c671eeb6ff39
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2018-09-29 11:12:35 -0500

    Build RPM locally, as current user.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
-./autogen.sh
-./configure
-make dist
+#!/bin/zsh
+(
+	./autogen.sh
+	./configure
+	make dist
+	rm -rf RPM
+	mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+	rpmbuild                                                        \
+		-D"_topdir ${PWD}/RPM"                                  \
+		-D"_sourcedir ${PWD}"                                   \
+		-D"_specdir ${PWD}"                                     \
+		-ba                                                     \
+		zshlogger.spec
+) 2>&1 | tee bootstrap.log
